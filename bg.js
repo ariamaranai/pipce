580db10c1ad86c4875d4e92e5d81f405eb1caaae
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-09 12:04:51 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,1 +1,16 @@
-(({runtime:t,contextMenus:m,scripting:s,action:p})=>(t.onInstalled.addListener(()=>m.create({id:"",title:"Picture in picture",contexts:["page","video"]})),t=(a,b)=>(b||a).url[0]!="c"&&s.executeScript({target:b?{tabId:b.id,frameIds:[a.frameId]}:{tabId:a.id,allFrames:!0},world:"MAIN",func:async()=>{let d=document,v=d.getElementsByTagName("video"),i=v.length,r=Array(i);if(i){while(r[--i]=v[i].offsetWidth,i);(v=v[r.indexOf(Math.max(...r))])!=d.pictureInPictureElement&&v.readyState?v.disablePictureInPicture=!await v.requestPictureInPicture():d.exitPictureInPicture()}}}),m.onClicked.addListener(t),p.onClicked.addListener(t)))(chrome)
+{
+let f =(a, b)=> (b||a).url[0] !="c" && chrome.scripting.executeScript({
+  target: b ? {tabId: b.id, frameIds: [a.frameId]}: {tabId: a.id, allFrames: !0},
+  world:"MAIN",
+  func: ()=> {
+    let d = document, v = d.getElementsByTagName("video"), i = v.length, n = 0, w = 0, t= 0
+    if (i) {
+      while (w < (t = v[--i].offsetWidth) && (w = t, n = i), i);
+      (v = v[n]) != d.pictureInPictureElement ? (v.disablePictureInPicture = 0, v.requestPictureInPicture()) : d.exitPictureInPicture()
+    }
+  }
+})
+chrome.action.onClicked.addListener(f)
+chrome.contextMenus.onClicked.addListener(f)
+}
+chrome.runtime.onInstalled.addListener(()=> chrome.contextMenus.create({id:"", title:"Picture in picture", contexts:["page","video"]}))
