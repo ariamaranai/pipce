9fd8ffe075d945202e7213276b396ebf3a8ac711
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-10 19:28:00 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -8,14 +8,16 @@
       func: () => {
         let video = document.body.getElementsByTagName("video");
         let i = video.length;
-        let maxWidth = 0;
-        let width = 0;
         let index = 0;
         if (i) {
-          while (
-            width < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-            i
-          );
+          if (i > 1) {
+            let maxWidth = 0;
+            let width = 0;
+            while (
+              maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+              i
+            );
+          }
           (video = video[index]).addEventListener("enterpictureinpicture",
             e => e.stopImmediatePropagation(),
             { capture: !0, once: !0 }
