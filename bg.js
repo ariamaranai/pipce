52a6ce4cf710fcb6fb781e1c597c588b76941f90
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-03 22:55:27 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,36 +1,38 @@
 {
-  let run = async (a, b) =>
-    (!b && a.url[0] == "c") || chrome.scripting.executeScript({
-      target: b
-        ? { tabId: b.id, frameIds: [a.frameId] }
-        : { tabId: a.id, allFrames: !0 },
-      world: (await chrome.contentSettings.javascript.get({
-        primaryUrl: (b || a).url
-      })).setting == "allow" ? "MAIN" : "ISOLATED",
-      func: () => {
-        let d = document;
-        let video = d.body.getElementsByTagName("video");
-        let i = video.length;
-        let index = 0;
-        if (i) {
-          if (i > 1) {
-            let maxWidth = 0;
-            let width = 0;
-            while (
-              maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-              i
-            );
+  let run = (a, b) => {
+    if (!b && a.url[0] == "c") {
+      try {
+        chrome.scripting.executeScript({
+          target: b
+            ? { tabId: b.id, frameIds: [a.frameId] }
+            : { tabId: a.id, allFrames: !0 },
+          func: () => {
+            let d = document;
+            let video = d.body.getElementsByTagName("video");
+            let i = video.length;
+            let index = 0;
+            if (i) {
+              if (i > 1) {
+                let maxWidth = 0;
+                let width = 0;
+                while (
+                  maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+                  i
+                );
+              }
+              (video = video[index]).addEventListener("enterpictureinpicture",
+                e => e.stopImmediatePropagation(),
+                { capture: !0, once: !0 }
+              );
+              video != d.pictureInPictureElement
+                ? (video.disablePictureInPicture = 0 , video.requestPictureInPicture())
+                : d.exitPictureInPicture();
+            }
           }
-          (video = video[index]).addEventListener("enterpictureinpicture",
-            e => e.stopImmediatePropagation(),
-            { capture: !0, once: !0 }
-          );
-          video != d.pictureInPictureElement
-            ? (video.disablePictureInPicture = 0 , video.requestPictureInPicture())
-            : d.exitPictureInPicture();
-        }
-      }
-    });
+        });
+      } catch (e) {}
+    }
+  }
   chrome.action.onClicked.addListener(run);
   chrome.contextMenus.onClicked.addListener(run);
 }
