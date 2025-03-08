d1af6415631d963fbef24bef97a42ea3537286de
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-08 13:49:31 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,34 +1,32 @@
 {
   let run = (a, b) => {
-    try {
-      let tabId = (b || a).id;
-      chrome.scripting.executeScript({
-        target: b ? { tabId, frameIds: [a.frameId] } : { tabId, allFrames: !0 },
-        func: () => {
-          let d = document;
-          let video = d.body.getElementsByTagName("video");
-          let i = video.length;
-          let index = 0;
-          if (i) {
-            if (i > 1) {
-              let maxWidth = 0;
-              let width = 0;
-              while (
-                maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-                i
-              );
-            }
-            (video = video[index]).addEventListener("enterpictureinpicture",
-              e => (e.stopImmediatePropagation()),
-              1
+    let tabId = (b || a).id;
+    chrome.scripting.executeScript({
+      target: b ? { tabId, frameIds: [a.frameId] } : { tabId, allFrames: !0 },
+      func: () => {
+        let d = document;
+        let video = d.body.getElementsByTagName("video");
+        let i = video.length;
+        let index = 0;
+        if (i) {
+          if (i > 1) {
+            let maxWidth = 0;
+            let width = 0;
+            while (
+              maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+              i
             );
-            video != d.pictureInPictureElement
-              ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
-              : d.exitPictureInPicture();
           }
+          (video = video[index]).addEventListener("enterpictureinpicture",
+            e => (e.stopImmediatePropagation()),
+            1
+          );
+          video != d.pictureInPictureElement
+            ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
+            : d.exitPictureInPicture();
         }
-      });
-    } catch (e) {}
+      }
+    }).catch(() => 0);
   }
   chrome.action.onClicked.addListener(run);
   chrome.contextMenus.onClicked.addListener(run);
