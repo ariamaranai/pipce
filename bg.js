fa4d4355ac696f536aced2cac9cbcd6a04f4c273
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-04 13:42:12 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -5,27 +5,27 @@
       target: b ? { tabId, frameIds: [a.frameId] } : { tabId, allFrames: !0 },
       js: [{ code:
 `{
-  let d = document;
-  let video = d.body.getElementsByTagName("video");
-  let i = video.length;
-  let index = 0;
-  if (i) {
-    if (i > 1) {
-      let maxWidth = 0;
-      let width = 0;
-      while (
-        maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-        i
-      );
-    }
-    (video = video[index]).addEventListener("enterpictureinpicture",
-      e => (e.stopImmediatePropagation()),
-      1
+let d = document;
+let video = d.body.getElementsByTagName("video");
+let i = video.length;
+let index = 0;
+if (i) {
+  if (i > 1) {
+    let maxWidth = 0;
+    let width = 0;
+    while (
+      maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+      i
     );
-    video != d.pictureInPictureElement
-      ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
-      : d.exitPictureInPicture();
   }
+  (video = video[index]).addEventListener("enterpictureinpicture",
+    e => (e.stopImmediatePropagation()),
+    1
+  );
+  video != d.pictureInPictureElement
+    ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
+    : d.exitPictureInPicture();
+}
 }`    }]
     }).catch(() => 0);
   }
