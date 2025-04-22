a6a8386b843398918930fa8131dc121a1274d248
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-23 00:10:58 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -10,14 +10,12 @@ let video = d.body.getElementsByTagName("video");
 let i = video.length;
 let index = 0;
 if (i) {
-  if (i > 1) {
-    let maxWidth = 0;
-    let width = 0;
-    while (
-      maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-      i
-    );
-  }
+  let maxWidth = 0;
+  let width = 0;
+  while (
+    maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+    i
+  );
   (video = video[index]).addEventListener("enterpictureinpicture",
     e => (e.stopImmediatePropagation()),
     1
