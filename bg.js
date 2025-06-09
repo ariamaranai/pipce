201f8b48e64b14fff80f721f82787ab34ddc8d2b
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-09 17:29:28 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -4,26 +4,7 @@
     let frameId = b && a.frameId;
     chrome.userScripts.execute({
       target: frameId ? { tabId, frameIds: [frameId] } : { tabId, allFrames: !0 },
-      js: [{ code:
-`{
-  let d = document;
-  let video = d.body.getElementsByTagName("video");
-  let i = video.length;
-  let index = 0;
-  if (i) {
-    let maxWidth = 0;
-    let width = 0;
-    while (
-      maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-      i
-    );
-    (video = video[index]).addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1);
-    video != d.pictureInPictureElement
-      ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
-      : d.exitPictureInPicture();
-  }
-}`   
-      }]
+      js: [{ file: "video.js" }]
     }).catch(() => 0);
   }
   chrome.action.onClicked.addListener(run);
