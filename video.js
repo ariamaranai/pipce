e20bf9fca64233352d9dd361e45db95b5382b94f
a <106607480+ariamaranai@users.noreply.github.com>
2025-08-06 07:46:51 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -1,18 +1,29 @@
 {
   let d = document;
-  let video = d.body.getElementsByTagName("video");
-  let i = video.length;
-  let index = 0;
-  if (i) {
-    let maxWidth = 0;
-    let width = 0;
-    while (
-      video[--i].readyState &&
-      maxWidth < (width = video[i].offsetWidth) &&
-      (maxWidth = width, index = i),
-      i
-    );
-    (video = video[index]).addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1);
+  let videos = d.getElementsByTagName("video");
+  let videoLen = videos.length;
+  if (videoLen) {
+    let video = videos[0];
+    if (videoLen > 1) {
+      let { scrollingElement } = d; 
+      let cx = (innerWidth + scrollingElement.scrollLeft) / 2;
+      let cy = (innerHeight + scrollingElement.scrollTop) / 2; 
+      let minds = 2e9;
+      let i = 0;
+      while (i < videos.length) {
+        let _video = videos[i];
+        if (_video.readyState) {
+          let rect = _video.getBoundingClientRect();
+          let ds = Math.abs(cx - (rect.width / 2 + rect.x)) + Math.abs(cy - (rect.height / 2 + rect.y));
+          ds < minds && (
+            minds = ds,
+            video = _video
+          );
+        }
+        ++i;
+      }
+    }
+    video.addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1);
     video != d.pictureInPictureElement
       ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
       : d.exitPictureInPicture();
