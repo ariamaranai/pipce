fc3af4ccd710be432bc428c290f008ebd21c0c5a
a <106607480+ariamaranai@users.noreply.github.com>
2025-08-09 01:48:35 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -2,30 +2,28 @@
   let d = document;
   let videos = d.getElementsByTagName("video");
   let videoLen = videos.length;
-  if (videoLen) {
-    let video = videos[0];
-    if (videoLen > 1) {
-      let { innerWidth, innerHeight } = self;
-      let maxVisibleSize = 0;
-      let i = 0;
-      while (i < videos.length) {
-        let _video = videos[i];
-        if (_video.readyState) {
-          let { x, right, y, bottom } = _video.getBoundingClientRect();
-          let visibleSize = Math.max(Math.min(right, innerWidth) - Math.max(x, 0), 0) * Math.max(Math.min(bottom, innerHeight) - Math.max(y, 0), 0);
-          maxVisibleSize < visibleSize && (
-            maxVisibleSize = visibleSize,
-            video = _video
-          );
-        }
-        ++i;
+  let video = videos[0];
+  if (videoLen > 1) {
+    let { innerWidth, innerHeight } = self;
+    let maxVisibleSize = 0;
+    let i = 0;
+    while (i < videos.length) {
+      let _video = videos[i];
+      if (_video.readyState) {
+        let { x, right, y, bottom } = _video.getBoundingClientRect();
+        let visibleSize = Math.max(Math.min(right, innerWidth) - Math.max(x, 0), 0) * Math.max(Math.min(bottom, innerHeight) - Math.max(y, 0), 0);
+        maxVisibleSize < visibleSize && (
+          maxVisibleSize = visibleSize,
+          video = _video
+        );
       }
+      ++i;
     }
-    video && (
-      video.addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1),
-      video != d.pictureInPictureElement
-        ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
-        : d.exitPictureInPicture()
-    );
   }
+  video && (
+    video.addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1),
+    video != d.pictureInPictureElement
+      ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
+      : d.exitPictureInPicture()
+  );
 }
