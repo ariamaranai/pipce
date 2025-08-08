7cad3183f1d7a0160d8836fa6d7cabdb0d65d7da
a <106607480+ariamaranai@users.noreply.github.com>
2025-08-09 01:41:46 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -5,27 +5,27 @@
   if (videoLen) {
     let video = videos[0];
     if (videoLen > 1) {
-      let { scrollingElement } = d; 
-      let cx = (innerWidth + scrollingElement.scrollLeft) / 2;
-      let cy = (innerHeight + scrollingElement.scrollTop) / 2; 
-      let minds = 2e9;
+      let { innerWidth, innerHeight } = self;
+      let maxVisibleSize = 0;
       let i = 0;
       while (i < videos.length) {
         let _video = videos[i];
         if (_video.readyState) {
-          let rect = _video.getBoundingClientRect();
-          let ds = Math.abs(cx - (rect.width / 2 + rect.x)) + Math.abs(cy - (rect.height / 2 + rect.y));
-          ds < minds && (
-            minds = ds,
+          let { x, right, y, bottom } = _video.getBoundingClientRect();
+          let visibleSize = Math.max(Math.min(right, innerWidth) - Math.max(x, 0), 0) * Math.max(Math.min(bottom, innerHeight) - Math.max(y, 0), 0);
+          maxVisibleSize < visibleSize && (
+            maxVisibleSize = visibleSize,
             video = _video
           );
         }
         ++i;
       }
     }
-    video.addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1);
-    video != d.pictureInPictureElement
-      ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
-      : d.exitPictureInPicture();
+    video && (
+      video.addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1),
+      video != d.pictureInPictureElement
+        ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
+        : d.exitPictureInPicture()
+    );
   }
 }
