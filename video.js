7dc50bf0abdb520436a65ba187b8291ce40be24d
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-11 15:28:40 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -2,26 +2,30 @@
   let { Math, document, innerWidth, innerHeight } = self;
   let { max, min } = Math;
   let { fullscreenElement } = document;
-  let videos = (fullscreenElement ?? document).getElementsByTagName("video");
-  let video;
-  let maxVisibleSize = 0;
-  let i = 0;
-  while (i < videos.length) {
-    let _video = videos[i];
-    if (_video.readyState) {
-      let { x, right, y, bottom } = _video.getBoundingClientRect();
-      let visibleSize = max(min(right, innerWidth) - max(x, 0), 0) * max(min(bottom, innerHeight) - max(y, 0), 0);
-      maxVisibleSize < visibleSize && (
-        maxVisibleSize = visibleSize,
-        video = _video
-      );
+  let video = fullscreenElement;
+  if (video?.tagName != "VIDEO") {
+    let videos = (fullscreenElement ?? document).getElementsByTagName("video");
+    let maxVisibleSize = 0;
+    let i = 0;
+    while (i < videos.length) {
+      let _video = videos[i];
+      if (_video.readyState) {
+        let { x, right, y, bottom } = _video.getBoundingClientRect();
+        let visibleSize = max(min(right, innerWidth) - max(x, 0), 0) * max(min(bottom, innerHeight) - max(y, 0), 0);
+        maxVisibleSize < visibleSize && (
+          maxVisibleSize = visibleSize,
+          video = _video
+        );
+      }
+      ++i;
     }
-    ++i;
-  }
-  (video ??= fullscreenElement?.shadowRoot?.querySelector("video")) && (
+    video ??= fullscreenElement?.shadowRoot?.querySelector("video")
+  } else
+    video = video.readyState && video;
+
+  video &&
     video.addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1),
     video != document.pictureInPictureElement
       ? video.requestPictureInPicture(video.disablePictureInPicture = 0)
       : document.exitPictureInPicture()
-  );
 }
