ff922b64e7dd8d5111c698c37df16e314d304c32
a <106607480+ariamaranai@users.noreply.github.com>
2026-03-14 17:44:23 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -2,7 +2,7 @@
   let d = document;
   let video = d.fullscreenElement || d.scrollingElement;
   if (!(video instanceof HTMLVideoElement)) {
-    let videos = video.getElementsByTagName("VIDEO");
+    let videos = video.getElementsByTagName("video");
     let { max, min } = Math;
     let maxVisibleSize = 0;
     let i = 0;
@@ -18,7 +18,7 @@
       }
       ++i;
     }
-    video?.readyState || (video = video.shadowRoot?.querySelector("VIDEO"));
+    video?.readyState || (video = video.shadowRoot?.querySelector("video"));
   }
   video?.readyState && (
     video.addEventListener("enterpictureinpicture", e => e.stopImmediatePropagation(), 1),
